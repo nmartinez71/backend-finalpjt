45831cf9608297a9b20b365fd003e0f81e8517fa
nmartinez71 <nmartinez71@ivytech.edu>
2025-02-19 16:31:41 -0500

    backend implemented with mongodb and mongoose

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // Backend (Glitch) - server.js
-const path = require("path");
+const path = require("path"); 
 const express = require('express');
 const app = express();
 const router = express.Router()
+const bodyParser = require('body-parser')
+const Song = require("./models/songs")
 
-
+app.use(bodyParser.json())
+app.use(express.json())
 
 // Enable CORS for all routes
-var cors = require('cors')
+/* var cors = require('cors') */
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -17,7 +20,35 @@ app.use((req, res, next) => {
 // Your existing routes
 app.use("/api", router)
 
-router.get('/songs', (req, res) => {
+//get songs from db - nef
+router.get("/songs", async(req,res) => {
+  let query = {}
+  if (req.query.genre){
+    query = {genre : req.query.genre}
+  }
+
+  //to find all songs in a db just use the find() method within the try/catch thats built into mongoose
+  try {
+    const songs = await Song.find(query); // This will return a promise
+    res.json(songs); // Send the result as a response
+  } catch (err) {
+    res.status(400).send(err); // Handle any errors
+  }
+  /* Song.find(query, function(err, songs){
+    if (err){
+      res.status(400).send(err)
+    }
+    else{
+      res.json(songs)
+    }
+  }) */
+});
+
+
+
+//Old reouter.get method to get songs and send them to the frontend - Neftali
+
+/* router.get('/songs', (req, res) => {
   const songs = [
     {title: "We Found Love",
             artist: "Rihanna",
@@ -28,7 +59,7 @@ router.get('/songs', (req, res) => {
     { title: 'Song 2', artist: 'Artist 2', genre: 'Rock' },
   ];
   res.json(songs);
-});
+}); */
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
--- db.js
+++ db.js
@@ -1,3 +1,5 @@
+// Creates the database connection from MongoDB Atlas to our Backend - Neftali
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://sdev_finalpjt:finalpjt2025@final-pjt.fltqk.mongodb.net/?retryWrites=true&w=majority&appName=final-pjt',{useNewURLParser: true})
 
